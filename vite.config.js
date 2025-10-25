95486079e4389755409146402762173aca20c424
Maximillian Eugene <106041742+ray-caster@users.noreply.github.com>
2025-10-26 00:48:51 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 4300,
+    allowedHosts: [
+      'texevo.org',
+      'www.texevo.org',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
 })
 
